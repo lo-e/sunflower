e45178552747e6db7cb5054bfe30a46b811d41b3
loe <lo-e@outlook.com>
2022-05-12 18:20:33 +0800

    update

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 (function() {
+    @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
+
+    $('.abc').each(function (index, element) {
+        console.log('yes each')
+        return false
+    })
     console.log('v0.0.8')
 })()
