36924d58aa39afa4925a3e7248bbfbb61f2a0adf
loe <lo-e@outlook.com>
2022-05-12 17:38:21 +0800

    update

---
+++ test.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         0x
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       loe
+// @match        https://sunflower-land.com/play*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    console.log('abcxyz')
+})
