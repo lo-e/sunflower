1504e6b9606ec718321088183d01eb84fe0b5b66
loe <lo-e@outlook.com>
2022-05-12 18:24:42 +0800

    update

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 (function() {
-    @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
-
     $('.abc').each(function (index, element) {
         console.log('yes each')
         return false
