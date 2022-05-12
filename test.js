fe34ef27f15f885d0ab5cd2180db2a5c7cc0ab44
loe <lo-e@outlook.com>
2022-05-12 18:12:34 +0800

    update

--- test.js
+++ test.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         0x
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       loe
-// @match        https://www.baidu.com*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
-// ==/UserScript==
-
 (function() {
     console.log('v0.0.6')
 })()
