0252688e55521d73fbc06a448a557fd2d57cea2c
loe <lo-e@outlook.com>
2022-05-12 18:50:16 +0800

    update

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 (function() {
-    $('.btn_wr.s_btn_wr.bg').each(function (index, element) {
+    document.getElementsByClassName('bg-brown-600 p-0.5 text-white shadow-lg').each(function (index, element) {
         console.log('yes each')
         return false
     })
+
+    // $('.btn_wr.s_btn_wr.bg').each(function (index, element) {
+    //     console.log('yes each')
+    //     return false
+    // })
     console.log('v0.0.8')
 })()
